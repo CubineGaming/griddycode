d64319164c2a1a133d2af2b18f0af0d862b71a35
CubineGaming <max.eko23@gmail.com>
2025-05-07 18:17:04 -0700

    Create rs.lua

---
+++ Lua/Plugins/rs.lua
@@ -0,0 +1,12 @@
+highlight("const", "reserved")
+highlight("fn", "reserved")
+highlight("let", "reserved")
+highlight("mut", "reserved")
+highlight("+", "operator")
+highlight("-", "operator")
+highlight("*", "operator")
+highlight("/", "operator")
+highlight("%", "operator")
+
+
+add_comment("add some raw pointers trust")
